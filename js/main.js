2f4b37473b0756395f65240d84a74a5481b1b0ca
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2020-08-11 12:31:54 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+(function(){
+    'use strict';
+
+    //two way data biding(to UI)
+    var vm = new Vue({
+        el: '#app',
+        data: {
+            todos:[
+                'task 1',
+                'task 2',
+                'task 3'
+            ]
+        },
+        methods: {
+            addItem: function() {
+                this.todos.push(this.newItem);
+                this.newItem = '';
+            }
+        }
+    });
+})();
